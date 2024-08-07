11426d0e833399f77f80297ac968448b0d14fc36
luanfelipeantunes <140464814+luanfelipeantunes@users.noreply.github.com>
2024-08-06 23:24:22 -0300

    Iniciando frontend

---
+++ recruiting-laon-frontend/src/app/components/header/Header.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+
+function Header() {
+    return (
+        <header>
+            <div>
+                <Link to={'/'}>
+                    <span>Icone</span>
+                    <h1 className="semibold16"> Voltar </h1>
+                </Link>
+            </div>
+
+            div
+        </header>
+    );
+}
+
+export default Header;
