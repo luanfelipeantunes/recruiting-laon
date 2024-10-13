3b32515ca9fcf0988eb7cbc1faee56cac0a67265
Luan Felipe Antunes <luanfelipe12.antunes@gmail.com>
2024-10-13 12:03:20 -0300

    udpate schedule for run at daily at 00h00

--- recruiting-laon-backend/app/Console/Kernel.php
+++ recruiting-laon-backend/app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('favorites:verify')->everyMinute();
+        $schedule->command('favorites:verify')->dailyAt('00:00');
     }
 
     /**
